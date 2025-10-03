54160e61beb0271a59175578400595e368dad980
Jheype <joaopedroa2707@gmail.com>
2025-10-03 17:34:55 -0300

    build2

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'media.discordapp.net' },
+    ],
+  },
   reactStrictMode: true,
   experimental: {
     typedRoutes: true
